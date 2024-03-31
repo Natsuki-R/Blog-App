fca20833c8b020fcb6874ea712306c1a2cb0850b
Natsuko-r <shenjiaojun@gmail.com>
2024-03-31 10:55:30 +0900

    Add Featured and CategoryList HTML and CSS

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,19 +1,45 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
 
 const AuthLinks = () => {
 
   const status = "not authenticated";
+  const [open, setOpen] = useState(false)
 
   return (
     <>
       {
         status === "not authenticated"
-          ? <Link href="/login">login</Link>
+          ? <Link href="/login" className={styles.link}>login</Link>
           : <>
             <Link href="/write">write</Link>
             <span className={styles.link}>logout</span >
           </>
+      }
+      <div className={styles.burger} onClick={() => setOpen(p => !p)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {
+        open && (
+          <div className={styles.responsiveMenu}>
+            <Link href="/" className={styles.link}>Homepage</Link>
+            <Link href="/" className={styles.link}>Contact</Link>
+            <Link href="/" className={styles.link}>About</Link>
+            {
+              status === "not authenticated"
+                ? <Link href="/login" className={styles.link} >login</Link>
+                : <>
+                  <Link href="/write">write</Link>
+                  <span className={styles.link}>logout</span >
+                </>
+            }
+          </div>
+        )
       }
     </>
   );
